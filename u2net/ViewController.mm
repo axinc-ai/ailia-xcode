9f8a8c5bc49c48093aa211dba285dc60ab6c8f17
客野一樹 <kyakuno@axell.co.jp>
2020-11-05 17:38:54 +0900

    Refactoring

--- u2net/ViewController.mm
+++ u2net/ViewController.mm
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 
 #include "ailia.h"
+#include <vector>
 
 #define PRINT_ERR printf
 #define PRINT_OUT printf
@@ -65,10 +66,10 @@ static void ailia_u2net(unsigned char *rgb,unsigned int width,unsigned int heigh
     }
 
     unsigned int input_size = input_shape.x*input_shape.y*input_shape.z*input_shape.w*sizeof(float);
-    float *input_buf = (float*)malloc(input_size);
+    std::vector<float> input_buf = std::vector<float>(input_size/sizeof(float));
     PRINT_OUT("input shape %d %d %d %d %d\n", input_shape.x, input_shape.y, input_shape.z, input_shape.w, input_shape.dim);
 
-    preprocess(input_buf, input_shape.x, input_shape.y, rgb, width, height);
+    preprocess(&input_buf[0], input_shape.x, input_shape.y, rgb, width, height);
     
     AILIAShape output_shape;
     status = ailiaGetOutputShape(net, &output_shape, AILIA_SHAPE_VERSION);
@@ -79,19 +80,16 @@ static void ailia_u2net(unsigned char *rgb,unsigned int width,unsigned int heigh
 
     PRINT_OUT("output shape %d %d %d %d %d\n", output_shape.x, output_shape.y, output_shape.z, output_shape.w, output_shape.dim);
     int preds_size = output_shape.x*output_shape.y*output_shape.z*output_shape.w*sizeof(float);
-    float *output_buf = (float*)malloc(preds_size);
+    std::vector<float> output_buf =std::vector<float>(preds_size);
 
     // inference
-    status = ailiaPredict(net, output_buf, preds_size, input_buf, input_size);
+    status = ailiaPredict(net, &output_buf[0], preds_size, &input_buf[0], input_size);
     if (status != AILIA_STATUS_SUCCESS) {
         PRINT_ERR("ailiaPredict failed %d\n", status);
         return;
     }
 
-    postprocess(rgb, width, height, output_buf, output_shape.x, output_shape.y);
-
-    free(input_buf);
-    free(output_buf);
+    postprocess(rgb, width, height, &output_buf[0], output_shape.x, output_shape.y);
 
     PRINT_OUT("Program finished successfully.\n");
 
@@ -113,10 +111,13 @@ static int preprocess(float *dst,unsigned int dst_width,unsigned int dst_height,
     }
     
     for(int y=0;y<dst_height;y++){
-        int sy=y*src_height/dst_height;
         for(int x=0;x<dst_width;x++){
             //channel last to channel first
+
+            //nearest neighbor
             int sx=x*src_width/dst_width;
+            int sy=y*src_height/dst_height;
+
             dst[y*dst_width+x+0*dst_width*dst_height]=(1.0*src[sy*src_width*4+sx*4+0]/max_value - 0.485) / 0.229;
             dst[y*dst_width+x+1*dst_width*dst_height]=(1.0*src[sy*src_width*4+sx*4+1]/max_value - 0.456) / 0.224;
             dst[y*dst_width+x+2*dst_width*dst_height]=(1.0*src[sy*src_width*4+sx*4+2]/max_value - 0.406) / 0.225;
@@ -143,9 +144,11 @@ static int postprocess(unsigned char *dst,unsigned int dst_width,unsigned int ds
     }
     
     for(int y=0;y<dst_height;y++){
-        int sy=y*src_height/dst_height;
         for(int x=0;x<dst_width;x++){
+            //nearest neighbor
             int sx=x*src_width/dst_width;
+            int sy=y*src_height/dst_height;
+
             float alpha_value = ((src[sy*src_width+sx]-min) / (max-min));
             for(int rgb=0;rgb<3;rgb++){
                 dst[y*dst_width*4+x*4+rgb] *= alpha_value;
@@ -167,25 +170,25 @@ static int postprocess(unsigned char *dst,unsigned int dst_width,unsigned int ds
     NSUInteger width = CGImageGetWidth(image);
     NSUInteger height = CGImageGetHeight(image);
     CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
-    unsigned char *rawData = (unsigned char *)malloc(height * width * 4);
+    std::vector<unsigned char> rawData = std::vector<unsigned char>(height * width * 4);
     NSUInteger bytesPerPixel = 4;
     NSUInteger bytesPerRow = bytesPerPixel * width;
     NSUInteger bitsPerComponent = 8;
-    CGContextRef context = CGBitmapContextCreate(rawData, width, height, bitsPerComponent, bytesPerRow, colorSpace, kCGImageAlphaPremultipliedLast | kCGBitmapByteOrder32Big);
+    CGContextRef context = CGBitmapContextCreate(&rawData[0], width, height, bitsPerComponent, bytesPerRow, colorSpace, kCGImageAlphaPremultipliedLast | kCGBitmapByteOrder32Big);
     CGColorSpaceRelease(colorSpace);
 
     CGContextDrawImage(context, CGRectMake(0, 0, width, height), image);
     CGContextRelease(context);
 
     // Predict
-    ailia_u2net(rawData,(unsigned int)width,(unsigned int)height);
+    ailia_u2net(&rawData[0],(unsigned int)width,(unsigned int)height);
 
     // Create new image from data buffer
     size_t bitsPerPixel             = CGImageGetBitsPerPixel(image);
 
     CGColorSpaceRef colorspace      = CGColorSpaceCreateDeviceRGB();
     CGBitmapInfo bitmapInfo         = CGImageGetBitmapInfo(image);
-    CGDataProviderRef provider      = CGDataProviderCreateWithData(NULL, rawData, height * width * 4, NULL);
+    CGDataProviderRef provider      = CGDataProviderCreateWithData(NULL, &rawData[0], height * width * 4, NULL);
 
     CGImageRef newImageRef = CGImageCreate (
                               width,
@@ -212,8 +215,6 @@ static int postprocess(unsigned char *dst,unsigned int dst_width,unsigned int ds
     [imageView setCenter:CGPointMake(pWidth/2, pHeight/2)];
 
     [self.view addSubview:imageView];
-
-    free(rawData);
 }
 
 
