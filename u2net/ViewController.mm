e4c732e6193bd2580601ee8ab31957d180f36c14
客野一樹 <kyakuno@axell.co.jp>
2020-11-05 17:29:13 +0900

    Fix channel order

--- u2net/ViewController.mm
+++ u2net/ViewController.mm
@@ -40,7 +40,6 @@ static void ailia_u2net(unsigned char *rgb,unsigned int width,unsigned int heigh
         return;
     }
 
-//    PRINT_OUT("env_id: %d\n", env_ptr->id);
     PRINT_OUT("env_name: %s\n", env_ptr->name);
 
     status = ailiaOpenStreamFile(net, [path1 cStringUsingEncoding:1]);
@@ -58,42 +57,43 @@ static void ailia_u2net(unsigned char *rgb,unsigned int width,unsigned int heigh
         return;
     }
 
-        AILIAShape input_shape;
-        status = ailiaGetInputShape(net, &input_shape, AILIA_SHAPE_VERSION);
-        if (status != AILIA_STATUS_SUCCESS) {
-            PRINT_ERR("ailiaGetInputShape failed %d\n", status);
-            return;
-        }
-    //    PRINT_OUT("input shape %d %d %d %d %d\n",input_shape.x, input_shape.y, input_shape.z, input_shape.w, input_shape.dim);
-        unsigned int input_size = input_shape.x*input_shape.y*input_shape.z*input_shape.w*sizeof(float);
-        float *input_buf = (float*)malloc(input_size);
-    
-        preprocess(input_buf, input_shape.x, input_shape.y, rgb, width, height);
-    
-        AILIAShape output_shape;
-        status = ailiaGetOutputShape(net, &output_shape, AILIA_SHAPE_VERSION);
-        if (status != AILIA_STATUS_SUCCESS) {
-            PRINT_ERR("ailiaGetOutputShape failed %d\n", status);
-            return;
-        }
-    //    PRINT_OUT("output shape %d %d %d %d %d\n", output_shape.x, output_shape.y, output_shape.z, output_shape.w, output_shape.dim);
-        int preds_size = output_shape.x*output_shape.y*output_shape.z*output_shape.w*sizeof(float);
-        float *output_buf = (float*)malloc(preds_size);
+    AILIAShape input_shape;
+    status = ailiaGetInputShape(net, &input_shape, AILIA_SHAPE_VERSION);
+    if (status != AILIA_STATUS_SUCCESS) {
+        PRINT_ERR("ailiaGetInputShape failed %d\n", status);
+        return;
+    }
 
-        // inference
+    unsigned int input_size = input_shape.x*input_shape.y*input_shape.z*input_shape.w*sizeof(float);
+    float *input_buf = (float*)malloc(input_size);
+    PRINT_OUT("input shape %d %d %d %d %d\n", input_shape.x, input_shape.y, input_shape.z, input_shape.w, input_shape.dim);
 
-            status = ailiaPredict(net, output_buf, preds_size, input_buf, input_size);
-            if (status != AILIA_STATUS_SUCCESS) {
-                PRINT_ERR("ailiaPredict failed %d\n", status);
-                return;
-            }
+    preprocess(input_buf, input_shape.x, input_shape.y, rgb, width, height);
+    
+    AILIAShape output_shape;
+    status = ailiaGetOutputShape(net, &output_shape, AILIA_SHAPE_VERSION);
+    if (status != AILIA_STATUS_SUCCESS) {
+        PRINT_ERR("ailiaGetOutputShape failed %d\n", status);
+        return;
+    }
+
+    PRINT_OUT("output shape %d %d %d %d %d\n", output_shape.x, output_shape.y, output_shape.z, output_shape.w, output_shape.dim);
+    int preds_size = output_shape.x*output_shape.y*output_shape.z*output_shape.w*sizeof(float);
+    float *output_buf = (float*)malloc(preds_size);
+
+    // inference
+    status = ailiaPredict(net, output_buf, preds_size, input_buf, input_size);
+    if (status != AILIA_STATUS_SUCCESS) {
+        PRINT_ERR("ailiaPredict failed %d\n", status);
+        return;
+    }
 
     postprocess(rgb, width, height, output_buf, output_shape.x, output_shape.y);
 
-        free(input_buf);
-        free(output_buf);
+    free(input_buf);
+    free(output_buf);
 
-        PRINT_OUT("Program finished successfully.\n");
+    PRINT_OUT("Program finished successfully.\n");
 
     ailiaDestroy(net);
 }
@@ -102,36 +102,39 @@ static int preprocess(float *dst,unsigned int dst_width,unsigned int dst_height,
 {
     unsigned char max_value=1;
     for(int y=0;y<src_height;y++){
-        for(int x=0;x<src_width*4;x++){
-            unsigned char value=src[y*src_width*4+x];
-            if(max_value<value){
-                max_value=value;
+        for(int x=0;x<src_width;x++){
+            for(int rgb=0;rgb<3;rgb++){
+                unsigned char value=src[y*src_width*4+x*4+rgb];
+                if(max_value<value){
+                    max_value=value;
+                }
             }
         }
     }
+    
+    printf("%d ",max_value);
 
     for(int y=0;y<dst_height;y++){
+        int sy=y*src_height/dst_height;
         for(int x=0;x<dst_width;x++){
-            int sy=y*src_height/dst_height;
+            //channel last to channel first
             int sx=x*src_width/dst_width;
-            dst[y*dst_width*3+x*3+0]=(1.0*src[sy*src_width*4+sx*4+0]/max_value - 0.485) / 0.229;
-            dst[y*dst_width*3+x*3+1]=(1.0*src[sy*src_width*4+sx*4+1]/max_value - 0.485) / 0.229;
-            dst[y*dst_width*3+x*3+2]=(1.0*src[sy*src_width*4+sx*4+2]/max_value - 0.406) / 0.225;
+            dst[y*dst_width+x+0*dst_width*dst_height]=(1.0*src[sy*src_width*4+sx*4+0]/max_value - 0.485) / 0.229;
+            dst[y*dst_width+x+1*dst_width*dst_height]=(1.0*src[sy*src_width*4+sx*4+1]/max_value - 0.456) / 0.224;
+            dst[y*dst_width+x+2*dst_width*dst_height]=(1.0*src[sy*src_width*4+sx*4+2]/max_value - 0.406) / 0.225;
         }
     }
 
     return 0;
 }
 
-
-
 static int postprocess(unsigned char *dst,unsigned int dst_width,unsigned int dst_height,const float *src,unsigned int src_width,unsigned int src_height)
 {
     float min = FLT_MAX;
     float max = FLT_MIN;
     for(int y=0;y<src_height;y++){
-        for(int x=0;x<src_width*4;x++){
-            unsigned char value=src[y*src_width*4+x];
+        for(int x=0;x<src_width;x++){
+            float value=src[y*src_width+x];
             if(min>value){
                 min=value;
             }
@@ -141,11 +144,16 @@ static int postprocess(unsigned char *dst,unsigned int dst_width,unsigned int ds
         }
     }
     
+    printf("%f %f ",min,max);
+
     for(int y=0;y<dst_height;y++){
+        int sy=y*src_height/dst_height;
         for(int x=0;x<dst_width;x++){
-            int sy=y*src_height/dst_height;
             int sx=x*src_width/dst_width;
-            dst[y*dst_width*4+x*4+3] = ((src[sy*src_width+sx]-min) / (max-min))*255.0f;
+            float alpha_value = ((src[sy*src_width+sx]-min) / (max-min));
+            for(int rgb=0;rgb<3;rgb++){
+                dst[y*dst_width*4+x*4+rgb] *= alpha_value;
+            }
         }
     }
 
@@ -155,12 +163,10 @@ static int postprocess(unsigned char *dst,unsigned int dst_width,unsigned int ds
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    // Do any additional setup after loading the view.
-
+    // Load image
     UIImage *img = [UIImage imageNamed:@"input.png"];
 
-
-    // First get the image into your data buffer
+    // Get data buffer from loaded image
     CGImageRef image = [img CGImage];
     NSUInteger width = CGImageGetWidth(image);
     NSUInteger height = CGImageGetHeight(image);
@@ -175,38 +181,11 @@ static int postprocess(unsigned char *dst,unsigned int dst_width,unsigned int ds
     CGContextDrawImage(context, CGRectMake(0, 0, width, height), image);
     CGContextRelease(context);
 
-    // Now your rawData contains the image data in the RGBA8888 pixel format.
-    //int xx=0;
-    //int yy=0;
-    
-    //int byteIndex = (bytesPerRow * yy) + xx * bytesPerPixel;
-    //red = rawData[byteIndex];
-    //green = rawData[byteIndex + 1];
-    //blue = rawData[byteIndex + 2];
-    //alpha = rawData[byteIndex + 3];
-    
-    //for(int i=0;i<width*height*4;i++){
-        //printf("%d ",rawData[i]);   //BGRA
-    //    rawData[i]+=20;
-    //}
-    
-    ailia_u2net(rawData,width,height);
-    
-    
-    
+    // Predict
+    ailia_u2net(rawData,(unsigned int)width,(unsigned int)height);
 
-    
-    
-    
-    
-    
-    
-    
-    //size_t width                    = CGImageGetWidth(image);
-    //size_t height                   = CGImageGetHeight(image);
-    //size_t bitsPerComponent         = CGImageGetBitsPerComponent(image);
+    // Create new image from data buffer
     size_t bitsPerPixel             = CGImageGetBitsPerPixel(image);
-    //size_t bytesPerRow              = CGImageGetBytesPerRow(image);
 
     CGColorSpaceRef colorspace      = CGColorSpaceCreateDeviceRGB();
     CGBitmapInfo bitmapInfo         = CGImageGetBitmapInfo(image);
@@ -228,12 +207,13 @@ static int postprocess(unsigned char *dst,unsigned int dst_width,unsigned int ds
 
     UIImage *newImage   = [UIImage imageWithCGImage:newImageRef];
 
+    // Put image to view
     UIImageView *imageView = [[UIImageView alloc] initWithImage:newImage];
 
-     CGFloat pWidth = [UIScreen mainScreen].bounds.size.width;
-     CGFloat pHeight = [UIScreen mainScreen].bounds.size.height;
+    CGFloat pWidth = [UIScreen mainScreen].bounds.size.width;
+    CGFloat pHeight = [UIScreen mainScreen].bounds.size.height;
 
-     [imageView setCenter:CGPointMake(pWidth/2, pHeight/2)];
+    [imageView setCenter:CGPointMake(pWidth/2, pHeight/2)];
 
     [self.view addSubview:imageView];
 
